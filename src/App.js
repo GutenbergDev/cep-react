04d01387452ccbc972af711c28dd9d07bfbe5d69
Gutemberg Medeiros <gutenbergdevm@hotmail.com>
2021-10-22 18:48:08 -0300

    Estudando API com react.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Input from "./Input";
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Div2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+  height: 500px;
+`;
+
+const Cep = styled.input`
+  width: 80%;
+  height: 30px;
+  background-color: #ccc;
+  font-size: 1.2rem;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 20px;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  top: 3px;
+  right: 65px;
+  height: 24px;
+  padding: 0 10px;
+  border-radius: 20px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const Div3 = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  height: auto;
+  margin-top: 20px;
+`;
 
 function App() {
+
+  const [number, setNumber] = React.useState('');
+
+  async function handleClick(e) {
+    let NumeroDoCep = number;
+
+    const response = await fetch(`https://viacep.com.br/ws/${NumeroDoCep}/json/`);
+    const json = await response.json();
+    setNumber(json)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <Div2>
+        <Div3>
+          <Cep
+            value={number}
+            type="number"
+            onChange={(e)=> setNumber(e.target.value)} 
+            required
+          />
+          <Button onClick={handleClick}>Buscar</Button>
+        </Div3>
+
+        <Input number={number} />
+      </Div2>
+    </Div>
   );
 }
 
---
+++ src/Input.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Div2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  height: auto;
+  margin-top: 20px;
+`;
+
+function Input({ number }) {
+
+
+
+  return (
+    <Div>
+      <Div2>
+      <span>Logradouro: {number.logradouro}</span>
+      <span>Bairro: {number.bairro}</span>
+      <span>Localidade: {number.localidade}</span>
+      <span>UF: {number.uf}</span>
+      <span>CEP: {number.cep}</span>
+      </Div2>
+    </Div>
+  )
+}
+
+export default Input;
